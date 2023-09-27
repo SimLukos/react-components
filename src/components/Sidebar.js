7d106d259de8c46305a1861d38969bd8b2c7857a
Simonas Lukoševičius <simonaslukosevicius@Simonass-MacBook-Pro.local>
2023-09-27 10:06:33 +0300

    added modal page and table page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,8 @@ function Sidebar() {
     { label: 'Dropdown', path: '/' },
     { label: 'Accordion', path: '/accordion' },
     { label: 'Buttons', path: '/buttons' },
+    { label: 'Modal', path: '/modal' },
+    { label: 'Table', path: '/table' },
   ];
 
   const renderedLinks = links.map((link) => {
